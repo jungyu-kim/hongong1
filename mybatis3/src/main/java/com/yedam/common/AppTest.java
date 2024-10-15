3fc34745e87cf8a7b91eeeb4df539d345c1a9c41
admin <admin@DESKTOP-511O90H>
2024-10-15 17:49:42 +0900

    1015

--- mybatis3/src/main/java/com/yedam/common/AppTest.java
+++ mybatis3/src/main/java/com/yedam/common/AppTest.java
@@ -9,31 +9,25 @@ import com.yedam.vo.Member;
 
 public class AppTest {
 
-   public static void main(String[] args) {
-      //MemberDAO dao = new MemberDAO();
-	   SqlSession sqlSession = DataSource.getInstance().openSession();
-	   MemberMapper dao = sqlSession.getMapper(MemberMapper.class);
-	   //등록.
-	   Member mbr = new Member();
-	   mbr.setMemberId("test99");
-	   mbr.setMemberName("연습99");
-	   mbr.setPhone("010-1234-1234");
-	   mbr.setPassword("1234");
-	   
-	   if (dao.deleteMember(mbr.getMemberId()) == 1) {
-		   sqlSession.commit();
-	   }
-	   
-	   
-	  
-	  
-	   
-	   
-	   
-      List<Member> result = dao.members();
-      for(Member member : result) {
-         System.out.println(member.toString());
-      }
-   }
-   
+	public static void main(String[] args) {
+		// MemberDAO dao = new MemberDAO();
+		SqlSession sqlSession = DataSource.getInstance().openSession(true);
+		MemberMapper dao = sqlSession.getMapper(MemberMapper.class);
+		// 등록.
+		Member mbr = new Member();
+		mbr.setMemberId("test99aa4s4");
+		mbr.setMemberName("연습99");
+		mbr.setPhone("010-1234-1234");
+		mbr.setPassword("1234");
+
+		if (dao.insertMember(mbr) == 1) {
+			sqlSession.commit();
+		}
+
+		List<Member> result = dao.members();
+		for (Member member : result) {
+			System.out.println(member.toString());
+		}
+	}
+
 }
